5099b91ea46c0a342f7331dda75c980837ca7498
Patryk Kownacki <kownacki@sheridancollege.ca>
2025-02-10 08:58:23 -0500

    Added index.html with corresponding HTML structure. No styling just yet.

---
+++ src/main/java/ca/sheridancollege/patrykkownacki/bookstore_assign1_kownacki/BookstoreAssign1KownackiApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.patrykkownacki.bookstore_assign1_kownacki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreAssign1KownackiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreAssign1KownackiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/sheridancollege/patrykkownacki/bookstore_assign1_kownacki/BookstoreAssign1KownackiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.patrykkownacki.bookstore_assign1_kownacki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreAssign1KownackiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
