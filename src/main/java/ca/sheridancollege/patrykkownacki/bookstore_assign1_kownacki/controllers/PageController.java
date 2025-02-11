92eeb57dba9eba95ca9934ccc059b839d2819756
Patryk Kownacki <kownacki@sheridancollege.ca>
2025-02-10 20:36:15 -0500

    Successfully updated the amount of books to corresponding HTML, using Httpsession

--- src/main/java/ca/sheridancollege/patrykkownacki/bookstore_assign1_kownacki/controllers/PageController.java
+++ src/main/java/ca/sheridancollege/patrykkownacki/bookstore_assign1_kownacki/controllers/PageController.java
@@ -2,6 +2,7 @@ package ca.sheridancollege.patrykkownacki.bookstore_assign1_kownacki.controllers
 
 import ca.sheridancollege.patrykkownacki.bookstore_assign1_kownacki.beans.Book;
 import ca.sheridancollege.patrykkownacki.bookstore_assign1_kownacki.service.BookService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
@@ -22,6 +23,9 @@ public class PageController {
     @Autowired
     private BookService bookService;
 
+    int bookCounter= 0;
+    double sum = 0.0;
+
     @GetMapping("/")
     public String home() {
         return "index";
@@ -39,17 +43,26 @@ public class PageController {
         return "redirect:/availablebooks";
     }
     @GetMapping("/addToCart/{bookPrice}")
-    public String addToCart(@PathVariable Double bookPrice) {
-        System.out.println("Book with price " + bookPrice + " added to cart.");
+    public String addToCart(@PathVariable Double bookPrice, HttpSession session) {
+        bookCounter++;
+        //sum += bookPrice;
+        session.setAttribute("bookCounter", bookCounter);
+        //System.out.println("Total amount of books:  " + bookCounter + " sum: " + sum);
         return "redirect:/shoppingbooks";
     }
     @GetMapping("/shoppingbooks")
-    public String shoppingbooks(Model model) {
+    public String shoppingbooks(Model model, HttpSession session) {
+        Integer bookCounter = (Integer) session.getAttribute("bookCounter");
+        if (bookCounter == null) {
+            bookCounter = 0;
+        }
         model.addAttribute("books", bookService.bookList());
+        model.addAttribute("bookCounter", bookCounter);
         return "shoppingbooks";
     }
     @GetMapping("/checkout")
-    public String checkout() {
+    public String checkout(Model model) {
+        model.addAttribute("books", bookService.bookList());
         return "checkout";
     }
 }
